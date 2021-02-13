109a0c2e66ca2d8c75dd1e02cae31e7222b4ff5f
Andy Ciobanu <30950318+andycb479@users.noreply.github.com>
2021-02-13 18:23:24 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,113 @@
+package com.LFPC.Lab1;
+
+//Variant 26.
+//        VN={S, A, B, C}, VT={a, b, c, d},
+//        P={
+//        1. S-dA
+//        2. A-aB
+//        3. B-bC
+//        4. C-cB
+//        5. B-d
+//        6. C-aA
+//        7. A-b }
+
+import java.util.HashMap;
+class Value{
+    public String Terminal;
+    public String NonTerminal;
+
+    public Value(String terminal, String nonTerminal) {
+        Terminal = terminal;
+        NonTerminal = nonTerminal;
+    }
+
+    public Value(String terminal) {
+        Terminal = terminal;
+        NonTerminal = "$";
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        String regularGrammar = "VN={S, A, B, C}, VT={a, b, c, d},\n" +
+                "P={\n" +
+                "1. S-dA\n" +
+                "2. A-aB\n" +
+                "3. B-bC\n" +
+                "4. C-cB\n" +
+                "5. B-d\n" +
+                "6. C-aA\n" +
+                "7. A-b }";
+        HashMap<String, Value[]> hmap = grammarToHash(regularGrammar);
+
+        String input = "dabaabcd";//dabcd,dabaad,dabaabcd
+        String NonTerminal = "S";
+
+        for (int i = 0; i < input.length(); i++) {
+            NonTerminal = ParseValue(NonTerminal,input.charAt(i),hmap);
+            if (NonTerminal == "$" && (input.length()-1)-i!=0){
+                NonTerminal = null;
+                break;
+            }
+        }
+        if(NonTerminal == null || !NonTerminal.equals("$")){
+            System.out.println("Rejected");
+        }
+        else{
+            System.out.println("Accepted");
+        }
+    }
+
+    static String ParseValue(String NonTerminal, char terminal, HashMap<String, Value[]> hmap){
+        if (NonTerminal == null){
+            return null;
+        }
+        for (Value value: hmap.get(NonTerminal)) {
+            if(value.Terminal.charAt(0) == terminal){
+                return value.NonTerminal;
+            }
+        }
+        return null;
+    }
+
+
+     static HashMap<String, Value[]> grammarToHash(String input){
+
+            HashMap<String, Value[]> hmap = new HashMap<String, Value[]>();
+            int pos = input.indexOf("P");
+            String[] productions = input.substring(pos).split("\\.");
+            for (int i = 1; i < productions.length; i++) {
+             productions[i]= productions[i].replaceAll("\\s|[0-9]|\\{|}", "");
+
+                if(hmap.containsKey(productions[i].charAt(0)+"")){
+                    int len = hmap.get(productions[i].charAt(0)+"").length;
+                    Value[] array = hmap.get(productions[i].charAt(0)+"").clone();
+                    Value[] temp = new Value[len+1];
+                    for (int j = 0; j < len; j++) {
+                        temp[j] = hmap.get(productions[i].charAt(0)+"")[j];
+                    }
+                    if(productions[i].length()==4){
+                        temp[len] = new Value(productions[i].charAt(2)+"",productions[i].charAt(3)+"");
+                    }
+                    else if(productions[i].length()==3){
+                        temp[len] = new Value(productions[i].charAt(2)+"","$");
+                    }
+                    hmap.put(productions[i].charAt(0)+"",temp);
+                }
+                else{
+                    if(productions[i].length()==4){
+                        hmap.put(productions[i].charAt(0)+"", new Value[]{new Value(productions[i].charAt(2)+"",productions[i].charAt(3)+"")});
+
+                    }
+                    else if(productions[i].length()==3){
+                        hmap.put(productions[i].charAt(0)+"", new Value[]{new Value(productions[i].charAt(2)+"","$")});
+                    }
+                }
+
+            }
+
+            return hmap;
+     }
+}
