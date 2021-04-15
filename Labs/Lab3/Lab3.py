2f8a3b69b7dafe04a631e2a3be39b1a1dbc0056b
Andy Ciobanu <30950318+andycb479@users.noreply.github.com>
2021-04-15 16:58:42 +0300

    Greibach Normalization added

--- Labs/Lab3/Lab3.py
+++ Labs/Lab3/Lab3.py
@@ -14,18 +14,12 @@ import itertools
 import string
 from pprint import pprint
 
-testGrammar2 = {
-    "S": ["aB", "AC"],
-    "A": ["a", "ASC", "BC"],
-    "B": ["b", "bS"],
-    "C": ["BA", "@"],
-}
 
 initGrammar = {
     "S": ["aBA", "AB"],
     "A": ["d", "dS", "AbBA", "@"],
     "B": ["a", "aS", "A"],
-    "D": ["Aba", ],
+    "D": ["Aba"],
 }
 
 
@@ -194,15 +188,15 @@ def replaceTerminals(grammar, dictionaryOfReplaces, alphabet):
         newList = list()
         for production in grammar[key]:
             if len(production) > 1 and not production.isupper():
+                changed = production
                 for char in production:
                     if char.islower() and char not in dictionaryOfReplaces:
                         dictionaryOfReplaces[char] = alphabet[-1]
-                        changed = production.replace(char, alphabet[-1])
-                        newList.append(changed)
+                        changed = changed.replace(char, alphabet[-1])
                         alphabet.pop()
                     elif char.islower():
-                        changed = production.replace(char, dictionaryOfReplaces[char])
-                        newList.append(changed)
+                        changed = changed.replace(char, dictionaryOfReplaces[char])
+                newList.append(changed)
             else:
                 newList.append(production)
         grammar[key] = newList
@@ -235,18 +229,134 @@ def divideNonTerminals(grammar, dictionaryOfReplaces, alphabet):
         grammar[key] = newList
 
 
-def normalize(grammar):
+def normalizeToChomsky(grammar):
     dictionaryOfReplaces = dict()
     alphabet = list(set(string.ascii_uppercase) - set(grammar.keys()))
     alphabet.sort(reverse=True)
     replaceTerminals(grammar, dictionaryOfReplaces, alphabet)
-    print("Normalization Step 1: Replace terminals with NonTerminals ->\n", grammar)
+    print("Chomsky Normalization Step 1: Replace terminals with NonTerminals ->\n", grammar)
     divideNonTerminals(grammar, dictionaryOfReplaces, alphabet)
-    print("Normalization Step 2: Replace chunks with NonTerminals ->\n", grammar)
+    print("Chomsky Normalization Step 2: Replace chunks with NonTerminals ->\n", grammar)
     newDict = dict()
     for key in dictionaryOfReplaces:
         newDict[dictionaryOfReplaces[key]] = [key]
     grammar.update(newDict)
+
+
+def checkForDirectLeftRecursion(grammar):
+    keys = list()
+    for key in grammar:
+        for production in grammar[key]:
+            if production[0] == key:
+                keys.append(key)
+    return keys
+
+
+def checkProductionHead(grammar, key, head, prevkey):
+
+    if head.islower():
+        return
+    if key == head:
+        return prevkey
+    for production in grammar[head]:
+        if production[0].isupper():
+            if production[0] == head:
+                return
+            if production[0] == prevkey == key:
+                return head
+            elif production[0] == prevkey:
+                return
+            temp = checkProductionHead(grammar, key, production[0], head)
+            if temp:
+                return temp
+
+
+
+
+
+def checkForIndirectLeftRecursion(grammar):
+
+    for key in grammar:
+        for production in grammar[key]:
+            if production[0].isupper():
+                temp = checkProductionHead(grammar, key, production[0],key)
+                if temp and temp != key:
+                    newlist = list()
+                    for prod in grammar[temp]:#replace with all productions
+                        newlist.append(prod+production[1:])
+                    grammar[key].extend(newlist)
+                    if grammar[key].__contains__(production):
+                        grammar[key].remove(production)
+
+
+
+
+def removeDirectLeftRecursion(grammar,keys):
+
+    alphabet = list(set(string.ascii_uppercase) - set(grammar.keys()))
+    alphabet.sort(reverse=True)
+
+    for key in keys:
+        alphas = list()
+        betas = list()
+        replaceKey = alphabet[0]
+        newProductions = (list(),list())
+        productions = grammar[key]
+        for production in productions:
+            if production[0] == key:#alphas
+                newProductions[0].extend((production[1:], production[1:] + replaceKey))
+            else:#betas
+                newProductions[1].extend((production, production+replaceKey))
+
+        grammar[replaceKey] = newProductions[0]
+        grammar[key] = newProductions[1]
+        alphabet.pop(0)
+
+
+def replaceNTtoT(grammar):
+    flag = True
+    while flag:
+        flag = False
+        for key in grammar:
+            newList = list()
+            for production in grammar[key]:
+                if production[0].isupper():
+                    if all(i[0].islower() for i in grammar[production[0]]):
+                        for innerProd in grammar[production[0]]:
+                            if innerProd.islower():
+                                changed = production.replace(production[0],innerProd)
+                                newList.append(changed)
+                                flag = True
+                            elif innerProd[0].islower():
+                                newList.append(innerProd+production[1:])
+                                flag = True
+                    else:
+                        newList.append(production)
+
+                else:
+                    newList.append(production)
+            grammar[key] = newList
+        print(grammar)
+
+
+
+
+def normalizeToGreibach(grammar):
+
+    print("Transform indirect left recursion to direct left recursion")
+    checkForIndirectLeftRecursion(grammar)
+    print(grammar)
+    directLeftRecursionKeys = checkForDirectLeftRecursion(grammar)
+    print("Direct Left Recursion NonTerminals =", list(set(directLeftRecursionKeys)), "Remove ->")
+    removeDirectLeftRecursion(grammar, list(set(directLeftRecursionKeys)))
+    print(grammar)
+    print("Replace first NonTerminal with productions that have first char a terminal ->")
+    replaceNTtoT(grammar)
+    print(grammar)
+
+
+
+
 
 
 def transformGrammar(grammar):
@@ -262,8 +372,45 @@ def transformGrammar(grammar):
     print("Removed NonProductive NonTerminals ->\n", grammar)
     removeNonAccesible(grammar)
     print("Removed NonAccesible NonTerminals ->\n", grammar)
-    normalize(grammar)
-    print("Normalization Step 3: Add new transitions ->\n", grammar)
+    normalizeToChomsky(grammar)
+    print("Chomsky Normalization Step 3: Add new transitions ->\n", grammar)
+    normalizeToGreibach(grammar)
+    removeNonAccesible(grammar)
+    print("Removed NonAccesible NonTerminals ->\n", grammar)
+    # print("Greibach Normalization Step 1: Replace first nonterminals with a terminal ->\n", grammar)
 
+testGrammarGreibach = {
 
+    "S": ["AC"],
+    "C": ["a"],
+    "B": ["aA", "BC",],
+    "A": ["b", "SB"]
+
+}
+testChomsky = {
+    "S": ["SC","SA","ab"],
+    "C":["c"],
+    "A": ["b"]
+}
+
+testGrammarGreibach3 = {
+
+    "S": ["AC"],
+    "C": ["a"],
+    "B": ["aA", "BC","Ba"],
+    "A": ["b", "B"]
+
+}
+
+testGrammarGreibach2 = {
+    "S": ["AB"],
+    "B": ["c"],
+    "D": ["a"],
+    "G": ["AD",],
+    "A": ["GB", "a"]
+}
 transformGrammar(initGrammar)
+#transformGrammar(testChomsky)
+#transformGrammar(testGrammarGreibach)
+#normalizeToGreibach(testGrammarGreibach2)
+# normalizeToGreibach(testGrammarGreibach)
